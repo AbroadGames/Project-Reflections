c2291f660b57e97f99ba08d3430cdb245c795c03
AbroadGames <AbroadGames@users.noreply.github.com>
2018-06-20 13:57:12 +0100

    Moved on to next project
    Spent a long time on this project and have lost motivation for it....
    
    Missing features-
    
    Consistent volume
    Game Sounds
    Saving / loading
    Fullscreen button
    Possible unlock system

---
+++ Reflections_UnityProject/Assets/Scripts/HighScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+    [SerializeField] private Text lblHighscore;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+        lblHighscore.text = Persistence.publicscore.ToString();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
